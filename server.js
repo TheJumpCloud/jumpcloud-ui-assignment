cae249cf601c8b3b2f459a111a678148066601c7
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-23 19:25:09 -0600

    set header to allow requests from `ember server`

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ var
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+  next();
+});
 
 var data = {
   todos: require('./data/todos')
