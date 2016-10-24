6f598177a873fcfea42bf9f89a0d97bc00d60276
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-23 20:00:43 -0600

    fix typo

--- client/tests/unit/serializers/application-test.js
+++ client/tests/unit/serializers/application-test.js
@@ -1,8 +1,8 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('adapter', 'Unit | Serializer | adapter', {
+moduleForModel('adapter', 'Unit | Serializer | application', {
   // Specify the other units that are required for this test.
-  needs: ['serializer:adapter']
+  needs: ['serializer:application']
 });
 
 // Replace this with your real tests.
