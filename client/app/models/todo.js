314944cb8a5023e20041c30d35c91a137fdc3d4f
Eric Thomas <eric.l.m.thomas@gmail.com>
2016-10-24 10:22:02 -0600

    set defaultValue on `done`

--- client/app/models/todo.js
+++ client/app/models/todo.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   description: DS.attr('string'),
-  done: DS.attr('boolean')
+  done: DS.attr('boolean', { defaultValue: false })
 });
