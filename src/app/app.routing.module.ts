f454debfcb53017bd23c1fb4743150700bfde129
Robert Northrop <robert0414@gmail.com>
2018-03-05 12:34:33 -0700

    tsconfig and source files

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { TodoItemFormComponent } from './todo-item-form/todo-item-form.component';
+import { TodoItemListComponent } from './todo-item-list/todo-item-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+
+const todoRoutes: Routes = [
+  { path: '', redirectTo: '/list', pathMatch: 'full' },
+  { path: 'list', component: TodoItemListComponent },
+  { path: 'todo-item/:id', component: TodoItemComponent },
+  { path: 'add', component: TodoItemFormComponent },
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(todoRoutes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ src/app/custom-confirm-modal/confirm.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+@Component({
+  selector: 'app-confirm',
+  templateUrl: './confirm.component.html',
+  styleUrls: ['./confirm.component.scss'],
+  animations: [
+    trigger('confirm', [
+      transition('void => *', [
+        style({ transform: 'scale3d(.3, .3, .3)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale3d(.0, .0, .0)' }))
+      ])
+    ])
+  ]
+})
+export class ConfirmModalComponent implements OnInit {
+  @Input() closable = true;
+  @Input() visible: boolean;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor() { }
+
+  ngOnInit() { }
+
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+}
